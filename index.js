b89c09e770945e2671718d0d442c9668c0c8220d
shaileshmourya <shaileshmourya1995@gmail.com>
2018-09-01 17:49:49 +0530

    index changes

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ var app = express()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-routes(app);
-/*app.get("/", function(req, res){
+//routes(app);
+app.get("/", function(req, res){
 		console.log("hello world");
 		app.use(express.static('./routes/public'))
 	});
-*/
+
 var server = app.listen(8080, function(){
 	console.log("app running on port:", server.address().port);
 });
